3de5eea35926843af3c4d3a462b7f618f0dc17c0
miguel <metuxxman@gmail.com>
2014-11-30 15:04:07 -0800

    practicing css rules https://www.youtube.com/watch?v=RDy4alhk9LU&index=98&list=UUpzRDg0orQBZFBPzeXm1yNg

---
+++ ann/menu.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+<!-- comment -->
+<head>
+<style>
+body{
+	background: black;margin:0px;
+}
+nav{ 
+	  padding:24px;
+	  border: #999 1px dashed;
+	  margin: 20px;	
+	
+    
+}
+nav>a{
+
+
+	font-family:Arial,helvetica, sans-serif;
+	font-size:17px;
+	text-decoration :none;
+	
+	padding:12px 24px;
+ 	margin-right:10px;
+
+	background:#464637;
+	color:#B9B9C8;
+	border-radius:3px;	
+
+}
+
+</style>
+<title></title>
+<link rel="" href="">
+<script></script>
+</head>
+<body>
+<nav>
+	<a href="#">Home</a><a href="#">About</a><a href="#">Staff</a> 
+</nav>
+
+
+</body>
+</html>
