d2e383d8eeb9f24977202d7a9aa5b2ac57ecefd4
miguel <metuxxman@gmail.com>
2014-12-01 14:22:53 -0800

    Compleated Pure CSS ANIMATED MENU BUTTONS@adam khoury ;using Transitions

--- ann/menu.php
+++ ann/menu.php
@@ -1,8 +1,11 @@
 <!Doctype html>
-<!--   https://www.youtube.com/watch?v=RDy4alhk9LU&index=98&list=UUpzRDg0orQBZFBPzeXm1yNg 
+<!--  
+ SERVER: http://scriptreference.com/test/Codeblock/ann/menu.php
+	CSS: https://www.youtube.com/watch?v=RDy4alhk9LU&index=98&list=UUpzRDg0orQBZFBPzeXm1yNg 
 -->
 <head>
 <style>
+/* */
 body{
 	background: black;margin:0px;
 }
@@ -14,20 +17,36 @@ nav{
     
 }
 nav>a{
-
-
+/*Font style*/
+	overflow:auto;
 	font-family:Arial,helvetica, sans-serif;
 	font-size:17px;
 	text-decoration :none;
-	
+/*Display*/	
 	padding:12px 24px;
  	margin-right:10px;
 
 	background:#464637;
 	color:#B9B9C8;
 	border-radius:3px;	
-
-}
+/*Animations*/
+	-webkit-transition:background 0.5s linear 0s,color 0.5s linear 0s;
+	-ms-transition:background 0.5s linear 0s,color 0.5s linear 0s;
+	-moz-transition:background 0.5s linear 0s,color 0.5s linear 0s;
+	-o-transition:background 0.5s linear 0s,color 0.5s linear 0s;
+	transition:background 0.5s linear 0s,color 0.5s linear 0s;
+    }
+div#body{
+	background:#EFFFBF;
+	color:#000;
+	padding:80%;
+	
+	}
+nav>a:hover{ 
+	background-color:#6f8a00;
+	color:#FFF;
+	}
+	
 
 </style>
 <title></title>
@@ -36,9 +55,14 @@ nav>a{
 </head>
 <body>
 <nav>
-	<a href="#">Home</a><a href="#">About</a><a href="#">Staff</a> 
+	<a href="#">Home</a>
+	<a href="#">About</a>
+	<a href="#">Services</a>
+	<a href="#">Staff</a> 
 </nav>
-
+<div id="body">
+<div id="sprite"></div>
+</div>
 
 </body>
 </html>
